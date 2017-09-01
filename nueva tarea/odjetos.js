788932fe4d5fb899395cbfd93c018b556d193a35
unknown <_HelmutColin@H4012.ei.ing.edu.ni>
2017-09-01 17:46:36 -0600

    modificacion 4

--- nueva tarea/odjetos.js
+++ nueva tarea/odjetos.js
@@ -1,41 +1,46 @@
 	var persona={
 					nombre: "Helmut",
-  					apellido: "colindres"
+  					apellido: "colindres",
+                    edad:20
 					};
 
 				var estudiante={
 					nombre: "Lesbia",
- 					 apellido: "Lacayo"
+ 					 apellido: "Lacayo",
+                    edad:"40"
 					};
 
 				var empleado={
 					cargo:"Docente",
-					salario: "10,000"
+					salario: "10,000",
+                    
 					};
 
 
 				var clase={
-					nombre:"Programacion 2",
+					nombre:"Programacion2",
 					turno:"Sabatino"
 					};
 
 				var carrera={
 					nombre:"Ingeieria en computacion",
+                    año:"4to"
 					};
 
 				var arreglo=[];
 
 				for(var i=0 ; i<5 ;i++){
 					if(i==0){
-					  arreglo[i]=persona.nombre + " " + persona.apellido;
+					  arreglo[i]=persona.nombre + " " + persona.apellido+ " "+ persona.edad;
 					  }	else  if(i==1){
- 						arreglo[i]=estudiante.nombre + " " + estudiante.apellido;
+ 						arreglo[i]=estudiante.nombre + " " + estudiante.apellido + "" +estudiante.edad;
  						}else if(i==2)	{
  							arreglo[i]=empleado.cargo + " "+ empleado.salario;
  						}else if(i==3){
  							arreglo[i]=clase.nombre + " " + clase.turno;
  						}else if(i==4){
- 							arreglo[i]=carrera.nombre;
+ 							arreglo[i]=carrera.nombre + " "+
+                                carrera.año;
  						}
  
 				};
